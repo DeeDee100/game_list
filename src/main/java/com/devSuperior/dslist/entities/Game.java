9b4ce8083fce1ef3d0ff183263e3c5ce540d17bb
DeeDee100 <amanda_tinoco00@hotmail.com>
2023-07-11 14:52:26 -0300

    add repository

--- src/main/java/com/devSuperior/dslist/entities/Game.java
+++ src/main/java/com/devSuperior/dslist/entities/Game.java
@@ -13,33 +13,41 @@ import jakarta.persistence.Table;
 @Table(name = "tb_game")
 public class Game {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
 
-    @Column(name = "game_year")
-    private Integer year;
-    private String title;
-    private String genre;
-    private String platforms;
-    private String imgUrl;
-    private String shortDescription;
-    private String longDescription;
-
-    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+	
+    public Game() {
+    }
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-        super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;		
 	}
 
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
 
@@ -77,6 +85,14 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
